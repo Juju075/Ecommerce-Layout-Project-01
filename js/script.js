f0ef438f6f47ea0759c12e0949601afe9ac1abb8
elgato <amzfba.1bestbuy@gmail.com>
2022-09-07 22:39:56 +0200

    fin modal

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 //The purpose of "use strict" is to indicate that the code should be executed in "strict mode".
 "use strict";
- 
+
+//Select the first element that has a "target" attribute:
+//Wrapper target | button icon X | empty did
+const modal = document.querySelector("[data-modal]"),
+  modalCloseBtn = document.querySelector("[data-modal-close]"),
+  modalCloseOverlay = document.querySelector("[data-modal-overlay]");
+console.log(modal, modalCloseBtn, modalCloseOverlay);
+
+//feature: add class closed to close modal w css.
+const modalCloseFunc = function () {
+  modal.classList.add("closed");
+};
+
+//modalCloseFunc (effectue la fonction suivante)
+modalCloseOverlay.addEventListener("click", modalCloseFunc);
+modalCloseBtn.addEventListener("click", modalCloseFunc);
